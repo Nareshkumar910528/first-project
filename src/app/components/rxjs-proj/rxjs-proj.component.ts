bda4df06991ef312993e30ad2e94e8494cf119eb
Nareshkumar <nareshkumar@senwave.com.my>
2021-02-09 12:19:33 +0800

    Added JS array methods

--- src/app/components/rxjs-proj/rxjs-proj.component.ts
+++ src/app/components/rxjs-proj/rxjs-proj.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable, of, timer, throwError, BehaviorSubject } from 'rxjs';
-import { map, catchError, tap, } from 'rxjs/operators';
+import {map, catchError, tap, filter,} from 'rxjs/operators';
 import { IndividualDataService } from 'src/app/services/individual-data.service';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import {error} from '@angular/compiler/src/util';
@@ -28,8 +28,12 @@ const httpOptions = {
 })
 export class RxjsProjComponent implements OnInit {
   userInfo:any;
+  userInfo2:any;
   employeeInfo: any;
   mappedData: any;
+  slicedMappedData: any;
+  joinedStingOfMappedData: any;
+  concatNewArrayWithMappedData: any;
   val: number;
   timerCompleteIndication: string = 'Execution is completed.';
   data: any[] = [];
@@ -50,6 +54,7 @@ export class RxjsProjComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUserData();
+    this.getStreetListings();
   }
 
   getUserData() {
@@ -58,6 +63,7 @@ export class RxjsProjComponent implements OnInit {
       return throwError(err);
     })).subscribe((respond: string | any[]) => { //success
       this.userInfo = respond;
+      this.userInfo.sort((name1, name2) => name1.name.localeCompare(name2.name));
       if(respond.length == 0) {
         this.noOfData = 'No Data';
       } else {
@@ -76,11 +82,54 @@ export class RxjsProjComponent implements OnInit {
 
   getStreetListings() {
     this.individualDataService.getUserInfo().subscribe((respond) => {
-      this.userInfo = respond;
-      this.mappedData = this.userInfo.map(data => data.address.street + ', ' + data.address.geo.lat + ', ' + data.address.geo.lng);
-      this.mappedData.push('Penang' + ', ' + '5.4141' + ', ' + '100.3288');
-      this.mappedData.unshift('Kuala Lumpur' + ', ' + '3.1390' + ', ' + '101.6869');
+      this.userInfo2 = respond;
+      // this.mappedData = this.userInfo2.map(data => data.address.street + ', ' + data.address.geo.lat + ', ' + data.address.geo.lng);
+      this.mappedData = this.userInfo2.map(data => data.address.street);
       console.log('Street: ', this.mappedData);
+      this.mappedData.push('Penang'); //add an element to the end of an array
+      console.log('pushedMappedData: ', this.mappedData);
+      this.mappedData.pop(); //removes the last element of an array
+      console.log('poppedMappedData: ', this.mappedData);
+      this.mappedData.unshift('Kuala Lumpur'); //adds an element to the beginning of an array
+      console.log('unshiftedMappedData: ', this.mappedData);
+      this.mappedData.shift(); //removes the first element of an array
+      console.log('shiftedMappedData: ', this.mappedData);
+      this.slicedMappedData = this.mappedData.slice(1,8); //selects the elements starting at the given start argument and the end at argument, but it does not include the given end argument
+      console.log('slicedMappedData: ', this.slicedMappedData);
+      this.mappedData.splice(2,0,'Penang'); //add and remove elements from an array
+      console.log('splicedMappedData: ', this.mappedData);
+      console.log('indexedOfMappedData: ', this.mappedData.indexOf("Norberto Crossing")); //searches an array for an element value and returns its position
+      this.joinedStingOfMappedData = this.mappedData.join(' == '); //convert an array to a string
+      console.log('joinedStingOfMappedData: ', this.joinedStingOfMappedData); //specify the separator
+      console.log('toStringOfMappedData: ', this.mappedData.toString()); //convert an array to a string
+      let newArrayKL = ['Kuala Lumpur'];
+      let newArraySelangor = ['Selangor'];
+      let newArraySingapore = ['Singapore'];
+      this.concatNewArrayWithMappedData = this.mappedData.concat(newArrayKL, newArraySelangor, newArraySingapore); //merge arrays together. It returns a new array by concatenating existing arrays
+      console.log('concatNewArrayWithMappedData: ', this.concatNewArrayWithMappedData);
+      console.log('includesChecking: ', this.concatNewArrayWithMappedData.includes('Pahang')); //checks if an element is available within an array. Will return true or false
+      if (this.concatNewArrayWithMappedData.includes('Penang')) { //checks if an element is available within an array
+        console.log('exist')
+      } else {
+        console.log('not exist')
+      }
+      const findMappedData =  this.concatNewArrayWithMappedData.find(streetName => streetName == 'Singapore'); //returns the value of the first array element that passes a test function
+      console.log('findMappedData: ', findMappedData);
+      const findIndexMappedData = this.concatNewArrayWithMappedData.findIndex(streetName => streetName == 'Hoeger Mall'); //returns the index of the first array element that passes a test function instead of returning the value
+      console.log('findIndexMappedData: ', findIndexMappedData);
+      this.concatNewArrayWithMappedData.forEach(streetName => { //iterate over an array’s items
+        if (streetName === 'Penang') {
+          console.log('forEachMappedData: Penang is my hometown');
+        } else if (streetName === 'Selangor') {
+          console.log('forEachMappedData: Selangor is my second hometown');
+        } else if (streetName === 'Kuala Lumpur') {
+          console.log('forEachMappedData: Kuala Lumpur is the capital of Malaysia');
+        }
+      });
+      const filteredMappedData = this.concatNewArrayWithMappedData.filter(streetName => streetName !== 'Kattie Turnpike'); //filters the array, based on the function passed to it
+      console.log('filteredMappedData: ', filteredMappedData);
+      const sortedMappedData = filteredMappedData.sort((a,b) => a.localeCompare(b)); //sorts the items of an array
+      console.log('sortedMappedData: ', sortedMappedData);
     })
   }
 
